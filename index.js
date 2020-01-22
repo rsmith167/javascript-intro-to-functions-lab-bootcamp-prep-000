cb4e14550fbc62237d12eb29e4ebd8c28f33b056
Rwanda S <rwanda.ace@gmail.com>
2020-01-22 17:04:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(toLowerCase(string))
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
-  console.log(toUpperCase(string))
+  console.log(string.toLowerCase())
 }
